5626f96b782498ad52f091018fa9cd9f41c0d42c
k11463 <k11463@yahoo.com.tw>
2020-03-04 10:14:44 +0800

    * gateway_folder   fix api resolver because it should be use import instead of require

--- Gateway/index.js
+++ Gateway/index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 
-const {ApolloServer} = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./src/schema/index');
-const resolvers = require('./src/resolvers/index');
+
+import resolvers from './src/resolvers/index'
 // const UserAPI = require('./datasources/user');
 
 // set up any dataSources our resolvers need
@@ -22,6 +23,6 @@ const server = new ApolloServer({
 
 // Start our server if we're not in a test env.
 // if we're in a test env, we'll manually start it in a test
-server.listen({port: process.env.PORT || 4000}).then(({url}) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
 	console.log(`🚀 app running at ${url}`);
 });
